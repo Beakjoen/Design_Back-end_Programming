b533d12e03f3bb0b80f92846169950041c890ffd
Nguyễn Quốc Huy <162018744+Beakjoen@users.noreply.github.com>
2025-04-03 17:20:28 +0700

    First
    upload file

---
+++ CNTT17-02/ClassLesson/Lesson1/Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿//1.Khai bao bien
+int x;
+x = 3;
+Console.WriteLine("x = " + x);
+
+
+int? x; //x co the chua null
+x = null;
+Console.WriteLine("x = " + x);
+
+//2. Toan tu hop nhat null
+int? a = null;
+int b = a ?? 10;// neu a = null, thi b = 10
+Console.WriteLine("x = " + x);
+
+
+//3. Chuoi noi suy
+string ten = "Vuong";
+int tuoi = 18;
+string s = $"Chao ban {ten}, ban da {tuoi} tuoi";
+Console.WriteLine(s);
